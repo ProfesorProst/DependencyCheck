4e4f34138c51e7a9c9fd40eeaba02def41231715
Yevhen Melnichuk <profesorprost@gmail.com>
2019-10-31 20:51:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependencyCheckV4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
